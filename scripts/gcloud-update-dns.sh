54b0d4df9c6744d20a38db95d7360d48c71cebfe
Stian Grytøyr <stian@grytoyr.net>
2016-04-06 13:46:59 +0200

    Made update-dns skip the update if IPs are identical

--- scripts/gcloud-update-dns.sh
+++ scripts/gcloud-update-dns.sh
@@ -12,11 +12,16 @@ current_ip_row="$(echo "${current}" | grep "${fullname}" || true)"
 current_ip="$(echo ${current_ip_row} | awk '{print $4}')"
 current_ttl="$(echo ${current_ip_row} | awk '{print $3}')"
 
-(gcloud dns record-sets -z=${zone} transaction abort || true) 2> /dev/null
-gcloud dns record-sets -z=${zone} transaction start
-if [ "${current_ip}x" != "x" ]
+if [ "${ip}" != "${current_ip}" ]
 then
-  gcloud dns record-sets -z=${zone} transaction remove --name="${fullname}" --ttl="${current_ttl}" --type=A "${current_ip}"
+  (gcloud dns record-sets -z=${zone} transaction abort || true) 2> /dev/null
+  gcloud dns record-sets -z=${zone} transaction start
+  if [ "${current_ip}x" != "x" ]
+  then
+    gcloud dns record-sets -z=${zone} transaction remove --name="${fullname}" --ttl="${current_ttl}" --type=A "${current_ip}"
+  fi
+  gcloud dns record-sets -z=${zone} transaction add --name="${fullname}" --type=A --ttl=60 "${ip}"
+  gcloud dns record-sets -z=${zone} transaction execute
+else
+  echo "Internal host IP did not change, no update needed"
 fi
-gcloud dns record-sets -z=${zone} transaction add --name="${fullname}" --type=A --ttl=60 "${ip}"
-gcloud dns record-sets -z=${zone} transaction execute
