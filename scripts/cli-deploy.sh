380c0815c354c4a724d2f4e312bfca643cfa428d
Stian Grytøyr <stian@grytoyr.net>
2016-04-14 14:39:36 +0200

    Improved cli deploy script

--- scripts/cli-deploy.sh
+++ scripts/cli-deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 set -e
 
-cap $1
-
+capfile="/tmp/capfile-$(date '+%Y-%m-%d-%H-%M-%N')"
+curl -o "${capfile}" "https://bitbucket.org/levelupify/devops/raw/master/data_files/capfile?cachebuster=$(date '+%Y-%m-%d-%H-%M-%N')"
+cap -f "${capfile}" $1
+rm "${capfile}"
